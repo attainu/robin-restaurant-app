208a896b52cb617dc2fe264b1c1c7b1e425d753d
mdwamiqali-h-au6 <ak0vamik@gmail.com>
2020-06-26 16:37:07 +0530

    update

--- authServer.js
+++ authServer.js
@@ -17,35 +17,38 @@ app.use(bodyParser.json());
 //+++======================-------====MIDDLEWEARS======--------===================+++//
 //  ?? middlewear ,:-fn that modify incoming req data.
 
-const posts = [
-    {
-        username: 'admin',
-        title: 'post1'
-    },
-    {
-        username : 'customer',
-        title: 'post2'
-    }
-]
+let refreshTokens = []
 
-app.get ('/posts', authenticateToken, (req,res) => {
-    res.json(posts.filter(post => post.username === req.user.name))
+app.post('/token', (req, res) => {
+    const refreshToken = req.body.token
+    if (refreshToken == null) return res.sendStatus(401)
+    if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403)
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403)
+        const accessToken = generateAccessToken({ name : user.name})
+        res.json ({accessToken:accessToken})
+    })
 })
 
+app.delete('/logout', (req, res) => {
+    refreshTokens = refreshTokens.filter(token => token != req.body.token)
+    res.sendStatus(204)
+})
 
-function authenticateToken(req, res, next) {
-    const authHeader = req.headers['authorization']
-    const token = authHeader && authHeader.split(' ')[1]
-    if (token==null) return res.sendStatus(401)
+app.post ('/login', (req,res) =>{
+    //authenticate user
+    const username = req.body.username
+    const user = { name: username}
 
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if (err) return res.sendStatus(403)
-        req.user = user
-        next()
-    })
+    const accessToken = generateAccessToken(user)
+    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
+    refreshTokens.push(refreshToken)
+    res.json({accessToken: accessToken, refreshToken: refreshToken})
+})
+
+function generateAccessToken(user) {
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn : '60s'})
 }
-
-
 
 //morgan middlewear
 app.use(morgan('dev'));
